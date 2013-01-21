9676e587d5568a3811f7d61f536b745d381b5f4b
Olm <lemoal.olivier@gmail.com>
2013-01-21 22:32:33 +0100

    fist commit

---
+++ EnvoiSms.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+import urllib, urllib2
+
+
+class EnvoiSms(object):
+	'''
+		From http://api.orange.com/fr/api/sms-api/documentation
+	'''
+
+	url_api = 'http://run.orangeapi.com/sms/sendSMS.xml?'
+	params = {'id' : '', #clé api
+			'from' : '38100', #numéro d'envoi
+			'to' : '', #numéro de portable
+			'content' : '', #contenu
+			'long_text' : '1', #autoriser fragmentation du sms
+			'max_sms' : '10', #nombre de fragments max
+			'ack' : '0',
+
+		}
+
+	def __init__(self,api_key):
+		self.params['id'] = api_key
+
+
+	def envoi(self,numero,message):
+		self.params['to'] = numero
+		self.params['content'] = message
+		url = self.url_api + urllib.urlencode(self.params)
+		r = urllib2.urlopen(url)
+
